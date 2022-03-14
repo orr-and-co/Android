a848faa51daba88c81af07b930246849c7e2dd2b
ThomasFrau <32460955+ThomasFrau@users.noreply.github.com>
2022-03-14 12:29:11 +0000

    Typo Fix

--- FiveSecondCity/app/src/main/java/com/example/fivesecondcity/Article.kt
+++ FiveSecondCity/app/src/main/java/com/example/fivesecondcity/Article.kt
@@ -3,7 +3,7 @@ package com.example.fivesecondcity
 // TODO: Flesh this out, check what can be accessed via the API
 data class Article(
     val id: Int,
-    val publishedAt: Int,
+    val published_at: Double, // Not sure about the datatype here
     val image: String,
     val title: String,
     val content: String
