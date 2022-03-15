3489fc2974eae4361041e02283df55f43081b7f4
ThomasFrau <32460955+ThomasFrau@users.noreply.github.com>
2022-03-15 13:45:45 +0000

    Changed "Preferences" to "Interests"

---
+++ FiveSecondCity/app/src/main/java/com/example/fivesecondcity/InterestsFragment.kt
@@ -0,0 +1,23 @@
+package com.example.fivesecondcity
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class InterestsFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_interests, container, false)
+    }
+}
